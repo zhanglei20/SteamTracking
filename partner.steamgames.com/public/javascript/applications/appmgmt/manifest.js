632619bc8e27de26bd47a41670d7c5d43e43a029
Steam Database <freeman@steamdb.info>
2026-02-21 04:05:45 +0000

    1 files | manifest.js

--- partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
+++ partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "10460299";
+var CLSTAMP = "10467237";
 (() => {
   "use strict";
   var e,
