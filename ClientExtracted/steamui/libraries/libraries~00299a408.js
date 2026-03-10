4e8a88eddc727216ee276e1bb3d1ad091ba7858d
Steam Database <freeman@steamdb.info>
2026-03-10 01:07:14 +0000

    62 files | ClientContentLinux.txt, 1779.json, changelist.txt, chunk~2dcc5aaf7.js, libraries~00299a408.js, library.js, friendsui_sc_schinese.json, shared_czech.json, shared_finnish.json, shared_sc_schinese.json, shared_turkish.json, steamui_brazilian.json, steamui_bulgarian.json, steamui_czech.json, steamui_danish.json, steamui_finnish.json, steamui_french.json, steamui_german.json, steamui_greek.json, steamui_italian.json, steamui_japanese.json, steamui_koreana.json, steamui_latam.json, steamui_norwegian.json, steamui_polish.json, steamui_romanian.json, steamui_russian.json, steamui_sc_schinese.json, steamui_spanish.json, steamui_swedish.json, steamui_tchinese.json, steamui_thai.json, steamui_turkish.json, steamui_ukrainian.json, steamui_vietnamese.json, steam_client_osx, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win64, steam_client_ubuntu12, steam_client_win64, steam_cmd_linux, steam_cmd_osx, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win64, steam_cmd_win64, steamchina_publicbeta_osx, steamchina_publicbeta_win64, steamchina_win64, steamdeck_publicbeta, steamdeck_stable, crashhandler.txt, gameoverlayui.txt, libtier0_s.txt, libvideo.txt, steam.txt, steamclient.txt, steamservice.txt, steamui.txt

--- ClientExtracted/steamui/libraries/libraries~00299a408.js
+++ ClientExtracted/steamui/libraries/libraries~00299a408.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "10505001";
+var CLSTAMP = "10508970";
 /* Third-party software licenses can be found at licenses.txt */ (self.webpackChunksteamui =
   self.webpackChunksteamui || []).push([
   [41],
